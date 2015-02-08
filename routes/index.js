da38ebe24d1d7df5997c568d1eca7ff0f3ab7f40
jamesmgittins <jamesmgittins@gmail.com>
2015-02-08 22:32:36 +0000

    Added some basic database functionality

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+
+var searchHistory = mongoose.model('searchhistory',{query:String,time:Date});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { result:req.session.result, csrfToken:req.csrfToken() });
+  searchHistory.find().sort('-time').limit(3).exec(function(err, results){
+	res.render('index', { result:req.session.result, csrfToken:req.csrfToken(), history:results });
+  });
 });
 
 router.post('/testpost', function(req, res){
@@ -13,6 +18,12 @@ router.post('/testpost', function(req, res){
   		return res.status(500);
 
   	req.session.result = tweets.statuses;
+  	
+  	new searchHistory({query:req.body.text,time:Date.now()}).save(function(err){
+  		if (err)
+  			console.log(err);
+  	});
+
   	res.redirect('/');
   });
   
